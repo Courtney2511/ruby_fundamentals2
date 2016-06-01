efb57f78f4570dee1534ad11a9199373d9bb2807
Courtney Noonan <courtneynoonan@me.com>
2016-06-01 15:40:57 -0400

    add code to increase the cohort size

--- exercise7.rb
+++ exercise7.rb
@@ -5,9 +5,19 @@ students = {
 }
 
 def cohort_display(hash)
-  hash.each do |cohort, students|
-    puts "#{cohort}: #{students}"
+  hash.each do |cohort, size|
+    puts "#{cohort}: #{size}"
   end
+end
+
+students[:cohort4] = 43
+
+cohort_display(students)
+
+puts students.keys
+
+students.each do |cohort, size|
+  students[cohort] = size + (size) * 0.05
 end
 
 cohort_display(students)
