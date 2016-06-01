40ec7333c2b196dd763395b6d2abdb8e88bc2bdd
Courtney Noonan <courtneynoonan@me.com>
2016-06-01 15:13:29 -0400

    defined method to display student hash

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,13 @@
 students = {
-  :cohort1 => 34
-  :cohort2 => 42
+  :cohort1 => 34,
+  :cohort2 => 42,
   :cohort3 => 22
 }
+
+def cohort_display(hash)
+  hash.each do |cohort, students|
+    puts "#{cohort}: #{students}"
+  end
+end
+
+cohort_display(students)
