bc86aadddecffd71bb3e637158dc9721c87b7515
Courtney Noonan <courtneynoonan@me.com>
2016-06-01 14:38:58 -0400

    counted list items and checked for bananas

--- exercise6.rb
+++ exercise6.rb
@@ -9,3 +9,11 @@ def display_list(array)
 end
 
 display_list(grocery_list)
+
+puts grocery_list.count
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas."
+else
+  puts "You don't need to pick up bananas today."
+end
